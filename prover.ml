d39a6056fa61160e914fa5ac4a19c875303b6fb4
Ry Wiese <ry.wiese@gmail.com>
2018-11-27 20:24:07 -0600

    made the ocaml file

--- prover.ml
+++ prover.ml
@@ -1,11 +1,14 @@
-type const = int
+type const = Int of int
 type var = string
 type value = Const of const | Var of var
 
-type stmt =
-    | Equals of value * value * (value * const) list
-    | LessThan of var * var * (var * const) list
-    | Plus of var * var * var * (var * const) list
-    | Times of var * var * var * (var * const) list
+type expr =
+    | Equals of value * value
+    | LessThan of value * value
+    | Plus of value * value * value
+    | Times of value * value * value
+    | Exists of var * expr
+    | ForAll of var * expr
+    | And of stmt
 
-type sent = stmt
+let
