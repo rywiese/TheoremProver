71e72658e588777aced639147685839441986db3
Ry Wiese <ry.wiese@gmail.com>
2018-11-28 18:17:09 -0600

    expression and statement grammar

--- prover.ml
+++ prover.ml
@@ -1,14 +1,30 @@
-type const = Int of int
-type var = string
-type value = Const of const | Var of var
+type const = Inf | Int of int | Real of float
 
 type expr =
-    | Equals of value * value
-    | LessThan of value * value
-    | Plus of value * value * value
-    | Times of value * value * value
-    | Exists of var * expr
-    | ForAll of var * expr
-    | And of stmt
+    | Const of const
+    | Var of string
+    | Plus of expr * expr
+    | Times of expr * expr
 
-let
+type stmt =
+    | Equals of expr * expr
+    | LessThan of expr * expr
+    | Plus of expr * expr * expr
+    | Times of expr * expr * expr
+    | Not of stmt
+    | And of stmt * stmt
+    | Or of stmt * stmt
+    | Implies of stmt * stmt
+    | Exists of string * stmt
+    | ForAll of string * stmt
+
+let rec exprToString e =
+    match e with
+    | Const (Inf) -> "Infinity"
+    | Const (Int i) -> string_of_int i
+    | Const (Real r) -> string_of_float r
+    | Var v -> v
+    | Plus (e1,e2) -> (exprToString e1) ^ " + " ^ (exprToString e2)
+    | Times (e1,e2) -> (exprToString e1) ^ " * " ^ (exprToString e2)
+
+exprToString (Plus (Plus (Var "x", Times (Const (Int 4), Var "y")), Times (Const (Int 7), Const (Int 3))))
