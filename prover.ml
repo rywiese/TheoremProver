70062002f4a6c3f9aee010c02794601ea534f5c8
Ry Wiese <ry.wiese@gmail.com>
2018-11-28 22:41:35 -0600

    made some changes

--- prover.ml
+++ prover.ml
@@ -34,9 +34,17 @@ let rec stmtToString s =
     | Exists (v, s) -> "There exists " ^ v ^ " such that (" ^ (stmtToString s) ^ ")"
     | ForAll (v, s) -> "For all " ^ v ^ ", (" ^ (stmtToString s) ^ ")"
 
+let rec concat l1 l2 =
+    match l1 with
+    | [] -> l2
+    | h::t -> h::(concat t l2)
+
 let rec prove s kb =
     match s with
-    | Implies (s1,s2) -> ("Assume " ^ (stmtToString s1))::(prove s2 (s1::kb))
-    | _ -> [stmtToString s]
+    | ForAll (v, s') -> prove s' kb
+    | Exists (v, s') -> prove s' kb
+    | Implies (s1, s2) -> ("Assume " ^ (stmtToString s1))::(prove s2 (s1::kb))
+    | And (s1, s2) -> concat (("Proof of " ^ s1 ^ ":")::(prove s1 kb)) (("Proof of " ^ s2 ^ ":")::(prove s2 kb))
+    | _ -> ["Not sure yet"]
 
 prove (Implies (Equals (Var "x", Var "y"), Equals (Var "f(x)", Var "f(y)"))) []
