e9cc5978b0c940b67e285e725421e5d3d31f5d2f
Ry Wiese <ry.wiese@gmail.com>
2018-11-30 00:48:01 -0600

    cleaned up with comments

--- prover.ml
+++ prover.ml
@@ -1,44 +1,39 @@
+(* Set operations *)
 let rec isIn e l =
     match l with
     | [] -> false
     | h::t -> if e=h then true else isIn e t
-
 let rec remove e l =
     match l with
     | [] -> []
     | h::t ->
         if e=h then remove e t else h::(remove e t)
-
 let rec concat l1 l2 =
     match l1 with
     | [] -> l2
     | h::t -> h::(concat t l2)
-
 let rec intersection l1 l2 =
     match l1 with
     | [] -> []
     | h::t ->
         if isIn h l2 then h::(intersection t l2)
         else intersection t l2
-
 let rec union l1 l2 =
     match l1 with
     | [] -> l2
     | h::t -> h::(union t (remove h l2))
-
 let rec difference l1 l2 =
     match l2 with
     | [] -> l1
     | h::t -> difference (remove h l1) t
 
+(* Statement grammar *)
 type const = Int of int
-
 type expr =
     | Const of const
     | Var of string
     | Plus of expr * expr
     | Times of expr * expr
-
 type stmt =
     | True
     | False
@@ -51,13 +46,13 @@ type stmt =
     | Equals of expr * expr
     | LessThan of expr * expr
 
+(* Free variable functions *)
 let rec fvExpr e =
     match e with
     | Const c -> []
     | Var v -> [v]
     | Plus (e1, e2) -> union (fvExpr e1) (fvExpr e2)
     | Times (e1, e2) -> union (fvExpr e1) (fvExpr e2)
-
 let rec fvStmt s =
     match s with
     | ForAll (v, s') -> difference (fvStmt s') [v]
@@ -70,13 +65,13 @@ let rec fvStmt s =
     | LessThan (e1, e2) -> union (fvExpr e1) (fvExpr e2)
     | _ -> []
 
+(* Converting to strings for printing *)
 let rec exprToString e =
     match e with
     | Const (Int i) -> string_of_int i
     | Var v -> v
     | Plus (e1,e2) -> (exprToString e1) ^ " + " ^ (exprToString e2)
     | Times (e1,e2) -> (exprToString e1) ^ " * " ^ (exprToString e2)
-
 let rec stmtToString s =
     match s with
     | True -> "True"
@@ -90,6 +85,7 @@ let rec stmtToString s =
     | Exists (v, s) -> "There exists " ^ v ^ " such that (" ^ (stmtToString s) ^ ")"
     | ForAll (v, s) -> "For all " ^ v ^ ", (" ^ (stmtToString s) ^ ")"
 
+(* Converting a statement into CNF *)
 let rec elimImp s =
     match s with
     | ForAll (v, s') -> (ForAll (v, elimImp s'))
@@ -99,7 +95,6 @@ let rec elimImp s =
     | Or (s1, s2) -> Or (elimImp s1, elimImp s2)
     | Not (s') -> Not (elimImp s')
     | _ -> s
-
 let rec pushNot s =
     match s with
     | Not (ForAll (v, s')) -> Exists (v, pushNot (Not s'))
@@ -115,6 +110,9 @@ let rec pushNot s =
     | Or (s1, s2) -> Or (pushNot s1, pushNot s2)
     | Not (s') -> Not (pushNot s')
     | _ -> s
+let rec standardize s =
+    match s with
+    |
 
 let cnf s = pushNot (elimImp s)
 
